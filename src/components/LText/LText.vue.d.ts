f4e48ce8f721d9a9abe72b1f972a7cca44fad9ad
zlfnn <zlf123jy@163.com>
2023-05-23 02:14:18 +0000

    Deploying to master from  @ 96b5c8d3499cac368be9a4e82379be3103ba4420 🚀

---
+++ src/components/LText/LText.vue.d.ts
@@ -0,0 +1,280 @@
+declare const _default: import("vue").DefineComponent<{
+    text: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontSize: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontFamily: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontWeight: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontStyle: {
+        type: StringConstructor;
+        default: any;
+    };
+    textDecoration: {
+        type: StringConstructor;
+        default: any;
+    };
+    lineHeight: {
+        type: StringConstructor;
+        default: any;
+    };
+    textAlign: {
+        type: StringConstructor;
+        default: any;
+    };
+    color: {
+        type: StringConstructor;
+        default: any;
+    };
+    backgroundColor: {
+        type: StringConstructor;
+        default: any;
+    };
+    actionType: {
+        type: StringConstructor;
+        default: any;
+    };
+    url: {
+        type: StringConstructor;
+        default: any;
+    };
+    height: {
+        type: StringConstructor;
+        default: any;
+    };
+    width: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingLeft: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingRight: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingTop: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingBottom: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderStyle: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderColor: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderWidth: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderRadius: {
+        type: StringConstructor;
+        default: any;
+    };
+    boxShadow: {
+        type: StringConstructor;
+        default: any;
+    };
+    opacity: {
+        type: StringConstructor;
+        default: any;
+    };
+    position: {
+        type: StringConstructor;
+        default: any;
+    };
+    left: {
+        type: StringConstructor;
+        default: any;
+    };
+    top: {
+        type: StringConstructor;
+        default: any;
+    };
+    right: {
+        type: StringConstructor;
+        default: any;
+    };
+    isEditing: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    tag: {
+        type: StringConstructor;
+        default: string;
+    };
+}, {
+    styleProps: import("vue").ComputedRef<Partial<Readonly<Partial<import("../../defaultProps").CommonComponentProps & {
+        isEditing: boolean;
+    }>>>>;
+    handleClick: () => void;
+}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    text: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontSize: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontFamily: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontWeight: {
+        type: StringConstructor;
+        default: any;
+    };
+    fontStyle: {
+        type: StringConstructor;
+        default: any;
+    };
+    textDecoration: {
+        type: StringConstructor;
+        default: any;
+    };
+    lineHeight: {
+        type: StringConstructor;
+        default: any;
+    };
+    textAlign: {
+        type: StringConstructor;
+        default: any;
+    };
+    color: {
+        type: StringConstructor;
+        default: any;
+    };
+    backgroundColor: {
+        type: StringConstructor;
+        default: any;
+    };
+    actionType: {
+        type: StringConstructor;
+        default: any;
+    };
+    url: {
+        type: StringConstructor;
+        default: any;
+    };
+    height: {
+        type: StringConstructor;
+        default: any;
+    };
+    width: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingLeft: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingRight: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingTop: {
+        type: StringConstructor;
+        default: any;
+    };
+    paddingBottom: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderStyle: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderColor: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderWidth: {
+        type: StringConstructor;
+        default: any;
+    };
+    borderRadius: {
+        type: StringConstructor;
+        default: any;
+    };
+    boxShadow: {
+        type: StringConstructor;
+        default: any;
+    };
+    opacity: {
+        type: StringConstructor;
+        default: any;
+    };
+    position: {
+        type: StringConstructor;
+        default: any;
+    };
+    left: {
+        type: StringConstructor;
+        default: any;
+    };
+    top: {
+        type: StringConstructor;
+        default: any;
+    };
+    right: {
+        type: StringConstructor;
+        default: any;
+    };
+    isEditing: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    tag: {
+        type: StringConstructor;
+        default: string;
+    };
+}>>, {
+    text: string;
+    fontSize: string;
+    fontFamily: string;
+    fontWeight: string;
+    fontStyle: string;
+    textDecoration: string;
+    lineHeight: string;
+    textAlign: string;
+    color: string;
+    backgroundColor: string;
+    actionType: string;
+    url: string;
+    height: string;
+    width: string;
+    paddingLeft: string;
+    paddingRight: string;
+    paddingTop: string;
+    paddingBottom: string;
+    borderStyle: string;
+    borderColor: string;
+    borderWidth: string;
+    borderRadius: string;
+    boxShadow: string;
+    opacity: string;
+    position: string;
+    left: string;
+    top: string;
+    right: string;
+    isEditing: boolean;
+    tag: string;
+}>;
+export default _default;
--- src/components/LText/index.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue'
-import LText from './LText.vue'
-
-LText.install = (app: App): void => {
-  app.component(LText.name, LText)
-}
-
-export default LText
